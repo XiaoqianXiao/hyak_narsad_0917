4a8b01b0bb7e17dceec2b0fa5b113235baad4809
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-02 11:54:15 -0700

    move the submit.sh

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=1st_level
+#SBATCH --partition=ckpt
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=04:00:00
@@ -11,7 +12,7 @@ module load apptainer
 
 # Define paths
 BIDS_DIR="/gscratch/fang/narsad"
-CONTAINER_IMAGE="/mmfs1/home/xxqian/repos/hyak_narsad/1st_level.sif"
+CONTAINER_IMAGE="/mmfs1/home/xxqian/repos/apptainers/1st_level.sif"
 
 # Get subjects using Apptainer
 SUBJECTS=$(apptainer exec --bind $BIDS_DIR:/data $CONTAINER_IMAGE python3 /app/get_subjects.py)
@@ -21,6 +22,7 @@ for subj in $SUBJECTS; do
     sbatch <<EOT
 #!/bin/bash
 #SBATCH --job-name=fmri_${subj}
+#SBATCH --partition=ckpt
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=04:00:00
