dfed2288cf2ca7d05c8e922548683b9285ca9b13
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-02 14:47:36 -0700

    modify the submit code

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name=fmri_${subj}
-#SBATCH --partition=ckpt
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
-#SBATCH --time=04:00:00
-#SBATCH --output=logs/fmri_${subj}_%j.out
-#SBATCH --error=logs/fmri_${subj}_%j.err
 
 module load apptainer
 
@@ -17,6 +10,14 @@ SUBJECTS='N101'
 
 for subj in $SUBJECTS; do
     sbatch <<EOT
+
+#SBATCH --job-name=fmri_${subj}
+#SBATCH --partition=ckpt
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=04:00:00
+#SBATCH --output=logs/fmri_${subj}_%j.out
+#SBATCH --error=logs/fmri_${subj}_%j.err
 
 apptainer run --bind $BIDS_DIR:/data $CONTAINER_IMAGE --subject $subj
 EOT
