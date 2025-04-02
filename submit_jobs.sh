f2715d04912a56b262e12c0dafc88aa581768dcd
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-02 14:05:35 -0700

    modify the submit file

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-
-#SBATCH --job-name=1st_level
-#SBATCH --partition=ckpt
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
-#SBATCH --time=04:00:00
-#SBATCH --output=logs/%x_%j.out
-#SBATCH --error=logs/%x_%j.err
-
-module load apptainer
-
-# Define paths
-BIDS_DIR="/gscratch/fang/narsad"
-CONTAINER_IMAGE="/mmfs1/home/xxqian/repos/apptainers/1st_level.sif"
-
-# Get subjects dynamically
-# SUBJECTS=$(apptainer exec --bind $BIDS_DIR:/data $CONTAINER_IMAGE python3 /app/get_subjects.py)
-SUBJECTS='N101'
-
-for subj in $SUBJECTS; do
-    sbatch <<EOT
-#!/bin/bash
 #SBATCH --job-name=fmri_${subj}
 #SBATCH --partition=ckpt
 #SBATCH --cpus-per-task=4
@@ -30,6 +8,16 @@ for subj in $SUBJECTS; do
 #SBATCH --error=logs/fmri_${subj}_%j.err
 
 module load apptainer
+
+# Define paths
+BIDS_DIR="/gscratch/fang/narsad"
+CONTAINER_IMAGE="/mmfs1/home/xxqian/repos/apptainers/1st_level.sif"
+
+SUBJECTS='N101'
+
+for subj in $SUBJECTS; do
+    sbatch <<EOT
+
 apptainer run --bind $BIDS_DIR:/data $CONTAINER_IMAGE --subject $subj
 EOT
 done
