fad2d3b99a802e869819d6b659141e6c172b75ee
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-08 18:02:40 -0700

    add ckpt

--- run_1st_level.py
+++ run_1st_level.py
@@ -74,7 +74,7 @@ def create_slurm_script(sub, inputs, work_dir, output_dir, task):
     slurm_script = f"""#!/bin/bash
 #SBATCH --job-name=first_level_sub_{sub}
 #SBATCH --account=fang
-#SBATCH --partition=checkpoint
+#SBATCH --partition=ckpt
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem=20G
