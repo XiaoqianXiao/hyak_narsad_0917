a266aab5e64ac99d1310140f87f564839a4293d0
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-08 23:31:14 -0700

    updated -p

--- run_1st_level.py
+++ run_1st_level.py
@@ -75,7 +75,7 @@ def create_slurm_script(sub, inputs, work_dir, output_dir, task, container_path)
     slurm_script = f"""#!/bin/bash
 #SBATCH --job-name=first_level_sub_{sub}
 #SBATCH --account=fang
-#SBATCH --partition=ckpt-all
+#SBATCH --partition=cpu-g2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=20G
