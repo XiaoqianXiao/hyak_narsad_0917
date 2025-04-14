f5a43d7d12b469635d1456b40a029c18940a2081
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-13 23:08:48 -0700

    modified the group level scripts

--- launch_group_level.sh
+++ launch_group_level.sh
@@ -4,7 +4,7 @@ scripts_dir="/gscratch/scrubbed/fanglab/xiaoqian/NARSAD/work_flow/groupLevel/who
 
 #for phaseID in 2 3; do
 for phaseID in 2; do
-  for script in "scripts_dir"/*_phase$phaseID*randomise.sh; do
+  for script in "$scripts_dir"/*_phase$phaseID*randomise.sh; do
     echo "Submitting $script"
     sbatch "$script"
   done
