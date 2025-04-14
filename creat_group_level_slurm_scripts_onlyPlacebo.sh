2498ba954d2ad574d8809466437e19c031819ca4
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-14 15:29:21 -0700

    add only placebo scripts

--- creat_group_level_slurm_scripts_onlyPlacebo.sh
+++ creat_group_level_slurm_scripts_onlyPlacebo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Environment settings
-container_path="/gscratch/scrubbed/fanglab/xiaoqian/images/narsad-fmri_group_level_1.0.sif"
+container_path="/gscratch/scrubbed/fanglab/xiaoqian/images/narsad-fmri_group_level_placebo_1.0.sif"
 base_dir="/gscratch/scrubbed/fanglab/xiaoqian/NARSAD/work_flows"
-script_base_dir="${base_dir}/groupLevel/whole_brain"
+script_base_dir="${base_dir}/groupLevel/whole_brain/Placebo"
 
 tasks=("phase2" "phase3")
 contrasts=$(seq 1 31)
