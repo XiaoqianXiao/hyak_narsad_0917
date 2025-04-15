aa4ff18862d2cb8e893620b60bb159a0094ee3f5
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-15 10:09:57 -0700

    m

--- launch_group_level.sh
+++ launch_group_level.sh
@@ -3,7 +3,7 @@
 scripts_dir="/gscratch/scrubbed/fanglab/xiaoqian/NARSAD/work_flows/groupLevel/whole_brain"
 
 #for phaseID in 2 3; do
-for phaseID in 2; do
+for phaseID in 3; do
   for script in "$scripts_dir"/*_phase$phaseID*randomise.sh; do
     echo "Submitting $script"
     sbatch "$script"
