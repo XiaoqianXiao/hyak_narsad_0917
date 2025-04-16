096db3870d4013b2c1df9d27b5e6c7d069e92060
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-04-15 23:51:42 -0700

    run flameo

--- launch_group_level.sh
+++ launch_group_level.sh
@@ -4,7 +4,8 @@ scripts_dir="/gscratch/scrubbed/fanglab/xiaoqian/NARSAD/work_flows/groupLevel/wh
 
 #for phaseID in 2 3; do
 for phaseID in 3; do
-  for script in "$scripts_dir"/*_phase$phaseID*randomise.sh; do
+  #for script in "$scripts_dir"/*_phase$phaseID*randomise.sh; do
+  for script in "$scripts_dir"/*_phase$phaseID*flameo.sh; do
     echo "Submitting $script"
     sbatch "$script"
   done
