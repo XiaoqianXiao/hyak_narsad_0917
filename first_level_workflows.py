162b499db5c70eac546d628b9b93ae981ed84dc8
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2025-05-20 10:57:12 -0700

    update event_df

--- first_level_workflows.py
+++ first_level_workflows.py
@@ -266,7 +266,7 @@ def first_level_single_trial_LSS_wf(inputs, output_dir, hrf_model='dgamma'):
             input_units='secs',
             high_pass_filter_cutoff=100
         ), name='l1_spec',
-        iterfield=['trial_info']
+        iterfield=['session_info', 'realignment_parameters']
     )
 
     # 7) Level1Design: generate design matrices
@@ -318,7 +318,7 @@ def first_level_single_trial_LSS_wf(inputs, output_dir, hrf_model='dgamma'):
         (trial_node, lss_info, [('trial_idx_list', 'target_idx')]),
         (datasource, lss_info, [('events', 'events_file')]),
 
-        (lss_info, l1_spec, [('trial_info', 'trial_info')]),
+        (lss_info, l1_spec, [('trial_info', 'session_info')]),
         (runinfo, l1_spec, [('realign_file', 'realignment_parameters')]),
         (apply_mask, l1_spec, [('out_file', 'functional_runs')]),
 
